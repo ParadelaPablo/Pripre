05546fa68338ea00db0cb50710e272933fb920aa
Pablo Paradela <paradelapablo@gmail.com>
2023-01-23 18:30:23 +0100

    primera preentrega

---
+++ Pripre/pripre.js
@@ -0,0 +1,82 @@
+
+
+
+//ingresar salarios para sumarlos
+//let salario1 =prompt("Ingrese salario de agosto")
+//let salario2 =prompt("Ingrese salario de septiembre")
+//let salario3 =prompt("Ingrese salario de octubre")
+
+
+//let total =salario1 + salario2 + salario3
+
+//alert("Lleva ganado + "total" + en los últimos tres meses")
+
+
+//ingresar nombre y apellido
+//let nombre =prompt("Ingresar nombre");
+
+//let apellido =prompt("Ingresar apellido");
+
+//alert("Usted se llama " + nombre + " " + apellido);
+
+
+
+//Ingresar notas para sacar prmedio
+//let nota1 =parseInt(prompt("Ingresar la primera nota"));
+
+//let nota2 =parseInt(prompt("Ingresar la segunda nota"));
+
+//let nota3 =parseInt(prompt("Ingresar la tercera nota"));
+
+//let notaTotal =nota1 + nota2 + nota3;
+
+//let promedio = notaTotal / 3;
+
+//if(promedio >= 7){alert("Su promedio es " + promedio + " y ha aprobado el curso")
+//} else {alert("Su promedio es " + promedio + " por lo que no ha superado el curso")}
+
+
+//for (let i=1; i <= 20; i++) {
+    
+//    let ingresarNombre =prompt("Ingresar tu nombre");
+//    alert("Usted, " + ingresarNombre + " " + " obtiene el turno N" + i);
+
+//}
+
+//let entrada = prompt("Ingresar un nombre de pila algún Power Rangers de la primera generación");
+
+//while(entrada != "ESC" ){
+//    switch (entrada) {
+//    case "Jason":
+//    alert("Jason era el PR rojo");
+//    break;
+//    case "Zack":
+//    alert("Zack era el PR negro");
+//    break;
+//    case "Billy":
+//    alert("Billy era el PR azul");
+//    break;
+//    case "Trini":
+//    alert("Trini era la PR amarilla -RIP");
+//    break;
+//    case "Kimberly":
+//    alert("Kimberly era la PR rosa");
+//    break;
+ //   case "Tommy":
+ //   alert("Tommy era el PR verde - RIP");
+//   break;
+ //   default:
+ //   alert("¿Cómo puede ser que no te acuerdes de ninguno?")
+  //  break;
+  //  }
+  //  entrada = prompt("Ingresar un nombre de pila algún Power Rangers de la primera generación");
+//}
+
+
+const pr = {
+    nombre: "Jason",
+    color: "rojo",
+    dinosaurio : "tiranosaurio"
+}
+alert (pr)
+
